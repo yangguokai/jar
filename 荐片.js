1edf61e9b93ab6ebee26a2e9d947bcece5146aa9
yangguokai <106662092+yangguokai@users.noreply.github.com>
2023-08-18 10:44:40 +0800

    Add files via upload

--- 荐片.js
+++ 荐片.js
@@ -1,90 +1,147 @@
 var rule = {
-    title:'荐片',
-    host:'http://api2.rinhome.com',
-    // homeUrl:'https://yjpapipxblwdohpakljwg.hxhzs.com/api/tag/hand?code=unknown601193cf375db73d&channel=wandoujia',//网站的首页链接,用于分类获取和推荐获取
-    homeUrl:'/api/tag/hand?code=unknown601193cf375db73d&channel=wandoujia',//网站的首页链接,用于分类获取和推荐获取
-    // url:'/api/crumb/list?area=0&category_id=fyclass&page=fypage&type=0&limit=24&fyfilter',
-    url:'/api/crumb/list?area=0&page=fypage&type=0&limit=24&fyfilter',
-    class_name:'电视剧&综艺&电影&动漫&全部',     // 筛选 /api/term/ad_fenlei?limit=10&page=1
-    class_url:'2&4&1&3&0',
-    detailUrl:'/api/node/detail?channel=wandoujia&token=&id=fyid',//二级详情拼接链接(json格式用)
-    searchUrl:'/api/video/search?key=**&page=fypage',
-    searchable:2,
-    quickSearch:0,
-    filterable:1,
-    filter:{
-        "0":[{"key":"year","name":"年代","value":[{"n":"全部","v":"0"},{"n":"2023","v":"153"},{"n":"2022","v":"101"},{"n":"2021","v":"118"},{"n":"2020","v":"16"},{"n":"2019","v":"7"},{"n":"2018","v":"2"},{"n":"2017","v":"3"},{"n":"2016","v":"22"}]},{"key":"sort","name":"排序","value":[{"n":"热门","v":"hot"},{"n":"评分","v":"rating"},{"n":"更新","v":"update"}]}],
-        "1":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":"1"},{"n":"首推","v":"5"},{"n":"动作","v":"6"},{"n":"喜剧","v":"7"},{"n":"战争","v":"8"},{"n":"恐怖","v":"9"},{"n":"剧情","v":"10"},{"n":"爱情","v":"11"},{"n":"科幻","v":"12"},{"n":"动画","v":"13"}]},{"key":"year","name":"年代","value":[{"n":"全部","v":"0"},{"n":"2023","v":"153"},{"n":"2022","v":"101"},{"n":"2021","v":"118"},{"n":"2020","v":"16"},{"n":"2019","v":"7"},{"n":"2018","v":"2"},{"n":"2017","v":"3"},{"n":"2016","v":"22"}]},{"key":"sort","name":"排序","value":[{"n":"热门","v":"hot"},{"n":"评分","v":"rating"},{"n":"更新","v":"update"}]}],
-        "2":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":"2"},{"n":"首推","v":"14"},{"n":"国产","v":"15"},{"n":"港台","v":"16"},{"n":"日韩","v":"17"},{"n":"海外","v":"18"}]},{"key":"year","name":"年代","value":[{"n":"全部","v":"0"},{"n":"2023","v":"153"},{"n":"2022","v":"101"},{"n":"2021","v":"118"},{"n":"2020","v":"16"},{"n":"2019","v":"7"},{"n":"2018","v":"2"},{"n":"2017","v":"3"},{"n":"2016","v":"22"}]},{"key":"sort","name":"排序","value":[{"n":"热门","v":"hot"},{"n":"评分","v":"rating"},{"n":"更新","v":"update"}]}],
-        "3":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":"3"},{"n":"首推","v":"19"},{"n":"海外","v":"20"},{"n":"日本","v":"21"},{"n":"国产","v":"22"}]},{"key":"year","name":"年代","value":[{"n":"全部","v":"0"},{"n":"2023","v":"153"},{"n":"2022","v":"101"},{"n":"2021","v":"118"},{"n":"2020","v":"16"},{"n":"2019","v":"7"},{"n":"2018","v":"2"},{"n":"2017","v":"3"},{"n":"2016","v":"22"}]},{"key":"sort","name":"排序","value":[{"n":"热门","v":"hot"},{"n":"评分","v":"rating"},{"n":"更新","v":"update"}]}],
-        "4":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":"4"},{"n":"首推","v":"23"},{"n":"国产","v":"24"},{"n":"海外","v":"25"},{"n":"港台","v":"26"}]},{"key":"year","name":"年代","value":[{"n":"全部","v":"0"},{"n":"2023","v":"153"},{"n":"2022","v":"101"},{"n":"2021","v":"118"},{"n":"2020","v":"16"},{"n":"2019","v":"7"},{"n":"2018","v":"2"},{"n":"2017","v":"3"},{"n":"2016","v":"22"}]},{"key":"sort","name":"排序","value":[{"n":"热门","v":"hot"},{"n":"评分","v":"rating"},{"n":"更新","v":"update"}]}]
-    },
-    // filter_url:'sort={{fl.sort or "hot"}}&year={{fl.year or "0"}}',
-    filter_url:'sort={{fl.sort or "update"}}&year={{fl.year or "0"}}&category_id={{fl.cateId}}',
-	filter_def:{
-		0:{cateId:'0'},
-        1:{cateId:'1'},
-		2:{cateId:'2'},
-		3:{cateId:'3'},
-		4:{cateId:'4'}
+	title:'MP4电影[磁]',
+	host:'https://www.mp4us.com',
+        homeUrl: '/',
+	url: '/list/fyclass-fypage.html?',
+	filter_url:'{{fl.class}}',
+	filter:{
 	},
-    headers:{
-        'User-Agent':'jianpian-android/350',
-        'JPAUTH':'y261ow7kF2dtzlxh1GS9EB8nbTxNmaK/QQIAjctlKiEv'
-    },
-    timeout:5000,
-    limit:8,
-    play_parse:true,
-    play_json:[{
-        re:'*',
-        json:{
-            parse:0,
-            jx:0
-        }
-    }],
-    lazy:'',
-    // double:true,
-    图片来源:'@Referer=www.jianpianapp.com@User-Agent=jianpian-version353',
-    // 推荐:'json:data;room;*;cover;*;*',
-    推荐:'json:.video;*;*;*;*',
-    一级:'json:data;title;path;playlist.title;id',
-    二级:{
-        // title:'data.title',
-        title:'data.title;data.types[0].name',
-        // desc:';data.description;data.id',
-        desc:'data.score;data.year.title;data.area.title;data.actors[0].name;data.directors[0].name',
-        img:'data.thumbnail',
-        // content:'data.thumbnail',
-        content:'data.description',
-        is_json:1,
-        tabs:`js:
-            TABS = [];
-            if (html.data.have_ftp_ur == 1) {
-                TABS.push("边下边播超清版")
-            }
-            if (html.data.have_m3u8_ur == 1) {
-                TABS.push("在线点播普清版")
-            }
-        `,
-        lists:`js:
-            log(TABS);
-            LISTS = [];
-            TABS.forEach(function(tab) {
-                if (/边下边播/.test(tab)) {
-                    let ftp = html.data.new_ftp_list;
-                    let d = ftp.map(function(it) {
-                        return it.title + "$" + (/m3u8/.test(it.url) ? play_url + it.url : "tvbox-xg:" + it.url)
-                    });
-                    LISTS.push(d)
-                } else if (/在线点播/.test(tab)) {
-                    let m3u = html.data.new_m3u8_list;
-                    let d = m3u.map(function(it) {
-                        return it.title + "$" + (/m3u8/.test(it.url) ? play_url + it.url : "tvbox-xg:" + it.url)
-                    });
-                    LISTS.push(d)
-                }
-            });
-        `,
-    },
-    搜索:'json:data;*;thumbnail;mask;*',
+	searchUrl: '/search/**-1.html',
+	searchable:2,
+	quickSearch:0,
+	filterable:0,
+	headers:{
+		'User-Agent': 'PC_UA',
+         	'Cookie':''
+	},
+	timeout:5000,
+	class_name: '动作片&科幻片&爱情片&喜剧片&恐怖片&战争片&剧情片&纪录片&动画片&电视剧',
+	class_url: '1&2&3&4&5&6&7&8&9&10',
+	play_parse:true,
+	play_json:[{
+		re:'*',
+		json:{
+			parse:0,
+			jx:0
+		}
+	}],
+	lazy:'',
+	limit:6,
+	推荐:'div.index_update ul li;a&&Text;;b&&Text;a&&href',
+	一级:'div#list_all ul li;img.lazy&&alt;img.lazy&&data-original;span.update_time&&Text;a&&href',
+	二级:{
+		title:"div.article-header h1&&Text",
+		img:"div.article-header div.pic img&&src",
+		desc:'div.article-header div.text&&Text',
+		content:'div.article-related.info p&&Text',
+		tabs:`js:
+pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;
+TABS=[]
+var d = pdfa(html, 'ul.down-list&&li');
+let magnetIndex=0;
+let ed2kIndex=0;
+d.forEach(function(it) {
+	let burl = pdfh(it, 'a&&href');
+	log("burl >>>>>>" + burl);
+	if (burl.startsWith("magnet")){
+		let result = 'magnet' + magnetIndex;
+		magnetIndex = magnetIndex + 1;
+		TABS.push(result);
+	}
+});
+d.forEach(function(it) {
+	let burl = pdfh(it, 'a&&href');
+	log("burl >>>>>>" + burl);
+	if (burl.startsWith("ed2k")){
+		let result = 'ed2k' + ed2kIndex;
+		ed2kIndex = ed2kIndex + 1;
+		TABS.push(result);
+	}
+});
+log('TABS >>>>>>>>>>>>>>>>>>' + TABS);
+`,
+		lists:`js:
+log(TABS);
+pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;
+LISTS = [];
+var d = pdfa(html, 'ul.down-list&&li');
+TABS.forEach(function(tab) {
+	log('tab >>>>>>>>' + tab);
+	if (/^magnet/.test(tab)) {
+		let targetindex = parseInt(tab.substring(6));
+		let index = 0;
+		d.forEach(function(it){
+			let burl = pdfh(it, 'a&&href');
+			if (burl.startsWith("magnet")){
+				if (index == targetindex){
+					let title = pdfh(it, 'a&&Text');
+					log('title >>>>>>>>>>>>>>>>>>>>>>>>>>' + title);
+					log('burl >>>>>>>>>>>>>>>>>>>>>>>>>>' + burl);
+					let loopresult = title + '$' + burl;
+					LISTS.push([loopresult]);
+				}
+				index = index + 1;
+			}
+		});
+	}
+});
+TABS.forEach(function(tab) {
+	log('tab >>>>>>>>' + tab);
+	if (/^ed2k/.test(tab)) {
+		let targetindex = parseInt(tab.substring(4));
+		let index = 0;
+		d.forEach(function(it){
+			let burl = pdfh(it, 'a&&href');
+			if (burl.startsWith("ed2k")){
+				if (index == targetindex){
+					let title = pdfh(it, 'a&&Text');
+					log('title >>>>>>>>>>>>>>>>>>>>>>>>>>' + title);
+					log('burl >>>>>>>>>>>>>>>>>>>>>>>>>>' + burl);
+					let loopresult = title + '$' + burl;
+					LISTS.push([loopresult]);
+				}
+				index = index + 1;
+			}
+		});
+	}
+});
+`,
+
+	},
+	搜索:`js:
+pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;
+if (rule_fetch_params.headers.Cookie.startsWith("http")){
+	rule_fetch_params.headers.Cookie=fetch(rule_fetch_params.headers.Cookie);
+	let cookie = rule_fetch_params.headers.Cookie;
+	setItem(RULE_CK, cookie);
+};
+log('mp4us seach cookie>>>>>>>>>>>>>' + rule_fetch_params.headers.Cookie);
+let _fetch_params = JSON.parse(JSON.stringify(rule_fetch_params));
+//log("mp4us search params>>>>>>>>>>>>>>>" + JSON.stringify(_fetch_params));
+let search_html = request( HOST + '/search/' + KEY + '-1.html', _fetch_params)
+//log("mp4us search result>>>>>>>>>>>>>>>" + search_html);
+let d=[];
+//'div#list_all li;img.lazy&&alt;img.lazy&&src;div.text_info h2&&Text;a&&href;p.info&&Text',
+let dlist = pdfa(search_html, 'div#list_all li');
+dlist.forEach(function(it){
+	let title = pdfh(it, 'img.lazy&&alt');
+	if (searchObj.quick === true){
+		if (title.includes(KEY)){
+			title = KEY;
+		}
+	}
+	let img = pd(it, 'img.lazy&&src', HOST);
+	let content = pdfh(it, 'div.text_info h2&&Text');
+	let desc = pdfh(it, 'p.info&&Text');
+	let url = pd(it, 'a&&href', HOST);
+	d.push({
+		title:title,
+		img:img,
+		content:content,
+		desc:desc,
+		url:url
+		})
+});
+setResult(d);
+`,
 }
